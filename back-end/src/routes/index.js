4e94f4bcb760bb29c4513717ceae3e8c2733759b
Rafael <Rafael.hc2610@gmail.com>
2024-10-10 11:43:08 -0300

    (10/10) Adicionada rota /keep-alive no back-end

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import prisma from '../database/client.js'
 const router = Router()
 
 /* GET home page. */
@@ -6,4 +7,22 @@ router.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+// Esta rota será chamada por um cronjob
+  router.get('/keep-alive', async function(req, res){
+    try{
+      // Uma simples requisição ao BD, obtendo o número
+      // de usuários cadastrados
+
+      await prisma.user.count()
+
+      //HTTP 204: No Content
+      res.status(204).end()
+    }
+    catch(error){
+      console.error(error)
+
+      //HTTP 500: Internal Server Error
+      res.status(500).end()
+    }
+  })
 export default router
