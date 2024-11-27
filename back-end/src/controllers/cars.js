9761ac13302329c5343801e5eba821ad24ba1f9a
Rafael <Rafael.hc2610@gmail.com>
2024-11-26 23:46:53 -0300

    TRABALHO 2

--- back-end/src/controllers/cars.js
+++ back-end/src/controllers/cars.js
@@ -1,13 +1,19 @@
 import prisma from '../database/client.js'
+import Car from '../models/car.js'
+import { ZodError } from 'zod'
 
 const controller = {}     // Objeto vazio
 
-controller.create = async function(req, res) {
+controller.create = async function (req, res) {
   try {
-    // Preenche qual usuaário criou o carro com o id do usuário autenticado
+
+    // Chama a validação do Zod para o cliente
+    Car.parse(req.body)
+
+    // Preenche qual usuário criou o carro com o id do usuário autenticado
     req.body.created_user_id = req.authUser.id
 
-    // Preenche qual usua-ário modificou por último o carro com o id
+    // Preenche qual usuário modificou por último o carro com o id
     // do usuário autenticado
     req.body.updated_user_id = req.authUser.id
 
@@ -16,18 +22,22 @@ controller.create = async function(req, res) {
     // HTTP 201: Created
     res.status(201).end()
   }
-  catch(error) {
+  catch (error) {
     console.error(error)
+
+    // Se for erro de validação do Zod retorna
+    // HTTP 422: Unprocessable Entity
+    if (error instanceof ZodError) res.status(422).send(error.issues)
 
     // HTTP 500: Internal Server Error
     res.status(500).end()
   }
 }
 
-controller.retrieveAll = async function(req, res) {
+controller.retrieveAll = async function (req, res) {
   try {
 
-    const included_rels = req.query.include?.split(',') ?? []
+    const includedRels = req.query.include?.split(',') ?? []
 
     const result = await prisma.car.findMany({
       orderBy: [
@@ -36,16 +46,16 @@ controller.retrieveAll = async function(req, res) {
         { id: 'asc' }
       ],
       include: {
-        customer: included_rels.includes('customer'),
-        created_user: included_rels.includes('created_user'),
-        updated_user: included_rels.includes('updated_user')
+        customer: includedRels.includes('customer'),
+        created_user: includedRels.includes('created_user'),
+        updated_user: includedRels.includes('updated_user')
       }
     })
 
     // HTTP 200: OK (implícito)
     res.send(result)
   }
-  catch(error) {
+  catch (error) {
     console.error(error)
 
     // HTTP 500: Internal Server Error
@@ -53,26 +63,26 @@ controller.retrieveAll = async function(req, res) {
   }
 }
 
-controller.retrieveOne = async function(req, res) {
+controller.retrieveOne = async function (req, res) {
   try {
 
-    const included_rels = req.query.include?.split(',') ?? []
+    const includedRels = req.query.include?.split(',') ?? []
 
     const result = await prisma.car.findUnique({
       where: { id: Number(req.params.id) },
       include: {
-        customer: included_rels.includes('customer'),
-        created_user: included_rels.includes('created_user'),
-        updated_user: included_rels.includes('updated_user')
+        customer: includedRels.includes('customer'),
+        created_user: includedRels.includes('created_user'),
+        updated_user: includedRels.includes('updated_user')
       }
     })
 
     // Encontrou ~> retorna HTTP 200: OK (implícito)
-    if(result) res.send(result)
+    if (result) res.send(result)
     // Não encontrou ~> retorna HTTP 404: Not Found
     else res.status(404).end()
   }
-  catch(error) {
+  catch (error) {
     console.error(error)
 
     // HTTP 500: Internal Server Error
@@ -80,10 +90,13 @@ controller.retrieveOne = async function(req, res) {
   }
 }
 
-controller.update = async function(req, res) {
+controller.update = async function (req, res) {
   try {
 
-    // Preenche qual usua-ário modificou por último o carro com o id
+    // Chama a validação do Zod para o cliente
+    Car.parse(req.body)
+
+    // Preenche qual usuário modificou por último o carro com o id
     // do usuário autenticado
     req.body.updated_user_id = req.authUser.id
 
@@ -93,19 +106,23 @@ controller.update = async function(req, res) {
     })
 
     // Encontrou e atualizou ~> HTTP 204: No Content
-    if(result) res.status(204).end()
+    if (result) res.status(204).end()
     // Não encontrou (e não atualizou) ~> HTTP 404: Not Found
     else res.status(404).end()
   }
-  catch(error) {
+  catch (error) {
     console.error(error)
+
+    // Se for erro de validação do Zod retorna
+    // HTTP 422: Unprocessable Entity
+    if (error instanceof ZodError) res.status(422).send(error.issues)
 
     // HTTP 500: Internal Server Error
     res.status(500).end()
   }
 }
 
-controller.delete = async function(req, res) {
+controller.delete = async function (req, res) {
   try {
     await prisma.car.delete({
       where: { id: Number(req.params.id) }
@@ -114,8 +131,8 @@ controller.delete = async function(req, res) {
     // Encontrou e excluiu ~> HTTP 204: No Content
     res.status(204).end()
   }
-  catch(error) {
-    if(error?.code === 'P2025') {
+  catch (error) {
+    if (error?.code === 'P2025') {
       // Não encontrou e não excluiu ~> HTTP 404: Not Found
       res.status(404).end()
     }
